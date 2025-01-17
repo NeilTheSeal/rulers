a6b24e4f4f7b0e2d53982d94c057b5b8806bf898
NeilTheSeal <neil.hendren@gmail.com>
2025-01-17 09:50:24 -0700

    Finish chapter 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,12 +2,15 @@
 
 require_relative "rulers/array"
 require_relative "rulers/version"
+require_relative "rulers/routing"
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
+      return [404, { "Content-Type" => "text/html" }, []] if env["PATH_INFO"] == "/favicon.ico"
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
