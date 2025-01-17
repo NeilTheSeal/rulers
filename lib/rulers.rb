af5062d30b84e0dcc3897bd501e06adea9717083
NeilTheSeal <neil.hendren@gmail.com>
2025-01-17 08:31:44 -0700

    Edit lib/rulers.rb

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,5 +4,14 @@ require_relative "rulers/version"
 
 module Rulers
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Application
+    def call(env)
+      [
+        200,
+        { "content-type" => "text/html" },
+        ["Hello Rack contrib!"]
+      ]
+    end
+  end
 end
