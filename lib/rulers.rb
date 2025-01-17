5057ebc95b996fc890ac917f933d98b6f0dfdc1f
NeilTheSeal <neil.hendren@gmail.com>
2025-01-17 09:38:27 -0700

    Add routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,12 +8,22 @@ module Rulers
 
   class Application
     def call(env)
-      `echo debug > debug.txt`
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [
         200,
         { "content-type" => "text/html" },
-        ["Hello Rack contrib!"]
+        [text]
       ]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
